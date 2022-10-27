7e22e59d5e9091a72ae2059c661b8fd6a9a9c9ac
Aroldolocro <lolosoto321@gmail.comm>
2022-10-26 23:49:53 -0300

    client 1.0

---
+++ src/Components/Explorer/Explorer.js
@@ -0,0 +1,52 @@
+import "./Explorer.css";
+import Image3 from "../../Images/Image3.png";
+import Image4 from "../../Images/Image4.png";
+
+const Explorer = () => {
+  return (
+    <div className="Explorer-background">
+      <div className="Explorer-content">
+        <div className="Explorer-C-B1">
+          <div className="Explorer-C-B1B1">
+            <div className="Explorer-C-B1B1B1">
+              <img src={Image3} className="Explorer-img-1" alt="" />
+            </div>
+            <p className="Explorer-txt-1">Pizza</p>
+          </div>
+          <div className="Explorer-C-B1B1">
+            <div className="Explorer-C-B1B1B1">
+              <img src={Image4} className="Explorer-img-1" alt="" />
+            </div>
+            <p className="Explorer-txt-1">Hamburguesa</p>
+          </div>
+          <div className="Explorer-C-B1B1">
+            <div className="Explorer-C-B1B1B1">
+              <img src={Image3} className="Explorer-img-1" alt="" />
+            </div>
+            <p className="Explorer-txt-1">Pizza</p>
+          </div>
+          <div className="Explorer-C-B1B1">
+            <div className="Explorer-C-B1B1B1">
+              <img src={Image4} className="Explorer-img-1" alt="" />
+            </div>
+            <p className="Explorer-txt-1">Pizza</p>
+          </div>
+          <div className="Explorer-C-B1B1">
+            <div className="Explorer-C-B1B1B1">
+              <img src={Image3} className="Explorer-img-1" alt="" />
+            </div>
+            <p className="Explorer-txt-1">Pizza</p>
+          </div>
+        </div>
+        <div className="Explorer-C-B2">
+          <p className="Explorer-txt-2">Promociones</p>
+          <p className="Explorer-txt-2">Promociones</p>
+          <p className="Explorer-txt-2">Promociones</p>
+          <p className="Explorer-txt-2">Promociones</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explorer;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,7 +1,5 @@
 import "./HomePage.css";
-import Image1 from "../../Images/Image1.jpeg";
-import Image3 from "../../Images/Image3.png";
-import Image4 from "../../Images/Image4.png";
+import Explorer from "../../Components/Explorer/Explorer";
 
 const HomePage = () => {
   return (
@@ -11,49 +9,28 @@ const HomePage = () => {
         <div className="HomePage-C-B2">
           <div className="HomePage-C-B2B1">
             <div className="HomePage-C-B2B1B1">
-              <img src={Image1} className="HomePage-img-1" alt="" />
-            </div>
-            <div className="HomePage-C-B2B1B2">
-              <p className="HomePage-txt-1">Servi Gas Menú</p>
-              <div className="HomePage-C-B2B1B2B1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fillRule="currentColor"
-                  className="HomePage-svg-1"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
-                </svg>
-                <p className="HomePage-txt-2">Av Kennedy y Av Mariano Moreno</p>
+              <div className="HomePage-C-B2B1B1B1">
+                <p className="HomePage-txt-1">Servi Gas</p>
+                <div className="HomePage-C-B2B1B1B1B1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fillRule="currentColor"
+                    className="HomePage-svg-1"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                  </svg>
+                  <p className="HomePage-txt-2">
+                    Av Kennedy y Av Mariano Moreno
+                  </p>
+                </div>
               </div>
             </div>
+            <Explorer />
           </div>
-          <div className="HomePage-C-B2B2">
-            <div className="HomePage-C-B2B2B1">
-              <img src={Image3} className="HomePage-img-3" alt="" />
-            </div>
-            <div className="HomePage-C-B2B2B2">
-              <div className="HomePage-C-B2B2B2B1">
-                <p className="HomePage-txt-3">Desayunos</p>
-                <p className="HomePage-txt-4">Productos (8)</p>
-              </div>
-              <p className="HomePage-txt-5">😍</p>
-            </div>
-          </div>
-          <div className="HomePage-C-B2B2">
-            <div className="HomePage-C-B2B2B1">
-              <img src={Image4} className="HomePage-img-4" alt="" />
-            </div>
-            <div className="HomePage-C-B2B2B2">
-              <div className="HomePage-C-B2B2B2B1">
-                <p className="HomePage-txt-3">Platos</p>
-                <p className="HomePage-txt-4">Productos (12)</p>
-              </div>
-              <p className="HomePage-txt-5">🤩</p>
-            </div>
-          </div>
+          <div className="HomePage-C-B2B2"></div>
         </div>
       </div>
     </div>
